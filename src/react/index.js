62a945566df89ff39b84b5786d6c0f208ba8f4a1
aviadhahami <aviad.hahami@snyk.io>
2022-01-28 17:21:35 +0200

    generated stubs

---
+++ src/react/index.js
@@ -0,0 +1,10 @@
+function reactHandler() {
+    // TODO: Implemet
+    // we should either:
+    // 1/ redirect to rdirectTo
+    // 2/ present a passed component
+    // 3/ show a white page w/ message
+    // 4/ fallback to white page
+}
+
+module.exports = reactHandler
--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,5 +1,10 @@
 function vanilla(redirectTo = undefined, htmlTpl = undefined, message = undefined) {
-
+    // TODO: implement
+    // we should either:
+    // 1/ redirect to rdirectTo
+    // 2/ present htmlTpl passed to us as string
+    // 3/ show a white page w/ message
+    // 4/ fallback to white page
 }
 
 module.exports = vanilla;
